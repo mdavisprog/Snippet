b1141b709336f52bc8136596f2a0355f3156cc93
mdavis <mdavisprog@gmail.com>
2021-05-11 10:13:55 -0700

    Bottom dock tabbed container which is anchored to the bottom of the main UI control. Theme for RichTextLabel was modified to match the rest of the theme.

---
+++ Assets/Scripts/UI/Docks/BottomDock.gd
@@ -0,0 +1,7 @@
+class_name BottomDock
+extends TabContainer
+
+# Controls to be displayed on the bottom of the main viewport.
+
+# The output window used to display output from snippets.
+onready var Output: RichTextLabel = $Output
