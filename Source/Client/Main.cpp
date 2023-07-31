a3a208f9b36e48ceae839bd96931ddccf5325aab
mdavisprog <mdavisprog@gmail.com>
2023-07-31 08:25:07 -0700

    Base framework for starting up the client application.

--- Source/Client/Main.cpp
+++ Source/Client/Main.cpp
@@ -1,3 +1,6 @@
+#include "Frontend.h"
+#include "OctaneGUI/OctaneGUI.h"
+
 #include <cstdio>
 
 int main(int argc, char** argv)
@@ -5,6 +8,21 @@ int main(int argc, char** argv)
     (void)argc;
     (void)argv;
 
-    printf("Hello Snippet\n");
-    return 0;
+    const char* Json = R"({
+    "Theme": "Resources/Themes/Dark.json",
+    "Windows": {
+        "Main": {"Title": "Snippet", "Width": 1280, "Height": 720,
+            "MenuBar": {},
+            "Body": {"Controls": []}
+        }
+    }
+})";
+
+    OctaneGUI::Application Application;
+    Frontend::Initialize(Application);
+
+    std::unordered_map<std::string, OctaneGUI::ControlList> Controls;
+    Application.Initialize(Json, Controls);
+
+    return Application.Run();
 }
