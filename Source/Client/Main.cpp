1ba724d803bac218f8c6ba65f22ff16b5a753615
mdavisprog <mdavisprog@gmail.com>
2023-08-02 10:52:11 -0700

    Add menu bar with 'File' -> 'Quit' option.

--- Source/Client/Main.cpp
+++ Source/Client/Main.cpp
@@ -9,7 +9,11 @@ int main(int argc, char** argv)
     "Theme": "Resources/Themes/Dark.json",
     "Windows": {
         "Main": {"Title": "Snippet", "Width": 1280, "Height": 720,
-            "MenuBar": {},
+            "MenuBar": {"Items": [
+                {"Text": "File", "ID": "File", "Items": [
+                    {"Text": "Quit", "ID": "Quit"}
+                ]}
+            ]},
             "Body": {"Controls": []}
         }
     }
@@ -22,6 +26,11 @@ int main(int argc, char** argv)
     Application
         .SetCommandLine(argc, argv)
         .Initialize(Json, Controls);
+    
+    Controls["Main"].To<OctaneGUI::MenuItem>("File.Quit")->SetOnPressed([&](const OctaneGUI::TextSelectable&) -> void
+        {
+            Application.Quit();
+        });
 
     return Application.Run();
 }
